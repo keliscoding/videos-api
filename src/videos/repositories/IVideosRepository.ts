405abb3c016b70e4e023311c194bf53fc419cd5c
Kelly Castelo <kawokkun@gmail.com>
2022-06-13 11:16:14 -0300

    feat: implements method FindAll to videos repository interface

--- src/videos/repositories/IVideosRepository.ts
+++ src/videos/repositories/IVideosRepository.ts
@@ -4,6 +4,7 @@ import { CreateVideoDTO } from '../dtos/CreateVideoDTO';
 interface IVideosRepository {
   create({ title, description, url }: CreateVideoDTO): Promise<Video>;
   findVideoByUrl(url: string): Promise<Video>;
+  findAll(): Promise<Video[]>;
 }
 
 export { IVideosRepository };
