9d4288911339e3a1f5e60bdff1e4a53e1bda92b9
Kelly Castelo <kawokkun@gmail.com>
2022-06-13 10:04:23 -0300

    test: add new test to check all fields of create video

--- src/videos/useCases/CreateVideo/CreateVideoUseCase.test.ts
+++ src/videos/useCases/CreateVideo/CreateVideoUseCase.test.ts
@@ -11,12 +11,38 @@ describe('Create Video', () => {
   });
 
   it('should be able to create a new video', async () => {
-    const newVideo = await videosRepositoryInMemory.create({
+    const newVideo = await createVideoUseCase.execute({
       title: 'Video Test',
       description: 'This is a description',
       url: 'https://urltest.com',
     });
 
     expect(newVideo).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new video without all fields filled', async () => {
+    expect(async () => {
+      await createVideoUseCase.execute({
+        title: undefined,
+        description: 'This is a description',
+        url: 'https://urltest.com',
+      });
+    }).rejects.toBeInstanceOf(Error);
+
+    expect(async () => {
+      await createVideoUseCase.execute({
+        title: 'Video Test',
+        description: undefined,
+        url: 'https://urltest.com',
+      });
+    }).rejects.toBeInstanceOf(Error);
+
+    expect(async () => {
+      await createVideoUseCase.execute({
+        title: 'Video Test',
+        description: 'This is a description',
+        url: undefined,
+      });
+    }).rejects.toBeInstanceOf(Error);
   });
 });
