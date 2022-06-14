0862d4e76ca1f9609b641db36e42c0b0cbd59489
Kelly Castelo <kawokkun@gmail.com>
2022-06-14 13:35:31 -0300

    refactor: remove container folder from src

--- src/container/index.ts
+++
@@ -1,9 +0,0 @@
-import { container } from 'tsyringe';
-
-import { IVideosRepository } from '@src/videos/repositories/IVideosRepository';
-import { VideosRepositoryTypeorm } from '@src/videos/repositories/implementation/VideosRepositoryTypeorm';
-
-container.registerSingleton<IVideosRepository>(
-  'VideosRepository',
-  VideosRepositoryTypeorm,
-);
