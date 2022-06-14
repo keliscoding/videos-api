bef0cfae8c5776a0db97f603b349764f1b5f1ab0
Kelly Castelo <kawokkun@gmail.com>
2022-06-14 07:02:14 -0300

    test: add validating id failing test

--- src/videos/useCases/FindVideoById/FindVideoByIdUseCase.test.ts
+++ src/videos/useCases/FindVideoById/FindVideoByIdUseCase.test.ts
@@ -1,3 +1,4 @@
+import { AppError } from '../../../errors/AppError';
 import { VideosRepositoryInMemory } from '../../repositories/in-memory/VideosRepositoryInMemory';
 import { FindVideoByIdUseCase } from './FindVideoByIdUseCase';
 
@@ -20,5 +21,11 @@ describe('Find video by id', () => {
     const videoFound = await findVideoByIdUseCase.execute(newVideo.id);
 
     expect(videoFound).toMatchObject(newVideo);
+  });
+
+  it('should throw an error if id is non-existent', async () => {
+    expect(async () => {
+      await findVideoByIdUseCase.execute('123123123');
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
