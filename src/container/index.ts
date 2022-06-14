2cf45c652d98352b567c88f7d2ded5571e58f26a
Kelly Castelo <kawokkun@gmail.com>
2022-06-14 11:11:21 -0300

    refactor: fix typo in VideosRepositoryTypeorm filename

--- src/container/index.ts
+++ src/container/index.ts
@@ -1,7 +1,7 @@
 import { container } from 'tsyringe';
 
 import { IVideosRepository } from '@src/videos/repositories/IVideosRepository';
-import { VideosRepositoryTypeorm } from '@src/videos/repositories/implementation/VideoRepositoryTypeorm';
+import { VideosRepositoryTypeorm } from '@src/videos/repositories/implementation/VideosRepositoryTypeorm';
 
 container.registerSingleton<IVideosRepository>(
   'VideosRepository',
