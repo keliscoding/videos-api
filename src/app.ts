6c188bd882d20c846389f08a4b6a10bb0247ec22
Kelly Castelo <kawokkun@gmail.com>
2022-06-14 13:59:30 -0300

    feat: add swagger to project

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import 'reflect-metadata';
 import express from 'express';
 import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'yaml-js';
 
 import handleError from './middleware/handleError';
 import '@shared/container';
-
 import { router } from './routes';
 
 const app = express();
+const swaggerDocument = yaml.load('./swagger.yaml');
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(router);
 
