c31c55181a8e7ab14530915e0ea926cd1bc2ca33
Kelly Castelo <kawokkun@gmail.com>
2022-06-15 08:42:36 -0300

    feat: implements category repository in memory

---
+++ src/videos/repositories/in-memory/CategoryRepositoryInMemory.ts
@@ -0,0 +1,18 @@
+import { Category } from '@src/videos/infra/typeorm/entities/Category';
+import { ICategoryRepository } from '../ICategoryRepository';
+
+class CategoryRepositoryInMemory implements ICategoryRepository {
+  categories: Category[] = [];
+
+  async create(title: string): Promise<Category> {
+    const newCategory = new Category();
+
+    Object.assign(newCategory, { title });
+
+    this.categories.push(newCategory);
+
+    return newCategory;
+  }
+}
+
+export { CategoryRepositoryInMemory };
