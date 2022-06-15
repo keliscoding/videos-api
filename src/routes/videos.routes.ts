6f17d2a45cd341598fafbb5e1c0c62d391c54de7
Kelly Castelo <kawokkun@gmail.com>
2022-06-15 06:42:52 -0300

    feat: implements findAllVideos route

--- src/routes/videos.routes.ts
+++ src/routes/videos.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { CreateVideoController } from '@src/videos/useCases/CreateVideo/CreateVideoController';
+import { FindAllVideosController } from '@src/videos/useCases/FindAllVideos/FindAllVideosController';
 
 const videosRouter = Router();
 
 const createVideoController = new CreateVideoController();
+const findAllVideosController = new FindAllVideosController();
 
 videosRouter.post('/', createVideoController.handle);
+videosRouter.get('/', findAllVideosController.handle);
 
 export { videosRouter };
