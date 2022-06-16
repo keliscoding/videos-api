80dc55c721724fb72b3b5a7c1b347623de5f077e
Kelly Castelo <kawokkun@gmail.com>
2022-06-16 16:45:44 -0300

    test: add find videos by category use case test

---
+++ src/modules/videos/useCases/FindVideosByCategory/FindVideosByCategoryUseCase.test.ts
@@ -0,0 +1,47 @@
+import { ICategoryRepository } from '@modules/categories/repositories/ICategoryRepository';
+import { CategoryRepositoryInMemory } from '@modules/categories/repositories/in-memory/CategoryRepositoryInMemory';
+import { VideosRepositoryInMemory } from '@modules/videos/repositories/in-memory/VideosRepositoryInMemory';
+import { FindVideosByCategoryUseCase } from './FindVideosByCategoryUseCase';
+
+let categoryRepositoryInMemory: ICategoryRepository;
+let videosRepositoryInMemory: VideosRepositoryInMemory;
+let findVideosByCategoryUseCase: FindVideosByCategoryUseCase;
+
+describe('Find videos by category', () => {
+  beforeEach(() => {
+    categoryRepositoryInMemory = new CategoryRepositoryInMemory();
+    videosRepositoryInMemory = new VideosRepositoryInMemory();
+    findVideosByCategoryUseCase = new FindVideosByCategoryUseCase(
+      videosRepositoryInMemory,
+      categoryRepositoryInMemory,
+    );
+  });
+  it('should be able to list all videos that belong to a category', async () => {
+    const category = await categoryRepositoryInMemory.create(
+      'category to be tested',
+    );
+    await videosRepositoryInMemory.create({
+      title: 'video test1',
+      description: 'random description',
+      url: 'http://www.video.com/111',
+      categories: [category],
+    });
+    await videosRepositoryInMemory.create({
+      title: 'video test2',
+      description: 'random description',
+      url: 'http://www.video.com/222',
+      categories: [category],
+    });
+    await videosRepositoryInMemory.create({
+      title: 'video test1',
+      description: 'random description',
+      url: 'http://www.video.com/333',
+      categories: [category],
+    });
+    const videos = await videosRepositoryInMemory.findVideosByCategoryId(
+      category.id,
+    );
+
+    expect(videos).toHaveLength(3);
+  });
+});
