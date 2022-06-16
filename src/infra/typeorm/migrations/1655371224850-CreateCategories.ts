a7473fd2f5bc844a6539e05d484d6dc31d765a92
Kelly Castelo <kawokkun@gmail.com>
2022-06-16 06:25:40 -0300

    feat: add createCategories migration

---
+++ src/infra/typeorm/migrations/1655371224850-CreateCategories.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCategories1655371224850 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'categories',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('categories');
+  }
+}
