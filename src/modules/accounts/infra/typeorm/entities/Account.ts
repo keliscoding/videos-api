41444873f8910d13c7e4797ad2079542c90802a5
Kelly Castelo <kawokkun@gmail.com>
2022-06-17 14:37:04 -0300

    feat: add typeorm decorators to account

--- src/modules/accounts/infra/typeorm/entities/Account.ts
+++ src/modules/accounts/infra/typeorm/entities/Account.ts
@@ -1,10 +1,22 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 
+@Entity('accounts')
 class Account {
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   username: string;
+
+  @Column()
   email: string;
+
+  @Column()
   password: string;
+
+  @CreateDateColumn()
+  created_at?: Date;
 
   constructor() {
     if (!this.id) {
