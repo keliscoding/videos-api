6ba811bb95d7e893d664f6c78bc1c79db8b3a37d
Kelly Castelo <kawokkun@gmail.com>
2022-06-17 08:24:55 -0300

    test: add 'apply default category to video' test

--- src/modules/videos/useCases/CreateVideo/CreateVideoUseCase.test.ts
+++ src/modules/videos/useCases/CreateVideo/CreateVideoUseCase.test.ts
@@ -1,15 +1,24 @@
 import { AppError } from '@errors/AppError';
+import { ICategoryRepository } from '@modules/categories/repositories/ICategoryRepository';
+import { CategoryRepositoryInMemory } from '@modules/categories/repositories/in-memory/CategoryRepositoryInMemory';
 import { VideosRepositoryInMemory } from '@modules/videos/repositories/in-memory/VideosRepositoryInMemory';
 import { IVideosRepository } from '@modules/videos/repositories/IVideosRepository';
 import { CreateVideoUseCase } from './CreateVideoUseCase';
 
 let videosRepositoryInMemory: IVideosRepository;
+let categoriesRepositoryInMemory: ICategoryRepository;
 let createVideoUseCase: CreateVideoUseCase;
 
 describe('Create Video', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     videosRepositoryInMemory = new VideosRepositoryInMemory();
-    createVideoUseCase = new CreateVideoUseCase(videosRepositoryInMemory);
+    categoriesRepositoryInMemory = CategoryRepositoryInMemory.getInstance();
+    createVideoUseCase = new CreateVideoUseCase(
+      videosRepositoryInMemory,
+      categoriesRepositoryInMemory,
+    );
+
+    await categoriesRepositoryInMemory.create('free');
   });
 
   it('should be able to create a new video', async () => {
@@ -62,5 +71,15 @@ describe('Create Video', () => {
         url: 'https://urltest.com',
       });
     }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to add a default category to a video created without one', async () => {
+    const newVideo = await createVideoUseCase.execute({
+      title: 'Video Test',
+      description: 'This is a description',
+      url: 'https://urltest.com',
+    });
+    expect(newVideo.categories).toHaveLength(1);
+    expect(newVideo.categories).toMatchObject([{ title: 'free' }]);
   });
 });
