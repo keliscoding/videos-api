c11804a387b03a59ee3130f36bbf80f2bd2134ab
Kelly Castelo <kawokkun@gmail.com>
2022-06-17 08:22:12 -0300

    feat: create default category seed

---
+++ src/infra/typeorm/seed/defaultCategory.ts
@@ -0,0 +1,23 @@
+import { AppDataSource } from '@src/data-source';
+import { v4 as uuid } from 'uuid';
+
+async function createFreeCategory() {
+  await AppDataSource.initialize();
+
+  const id = uuid();
+
+  await AppDataSource.manager.query(
+    `
+        INSERT INTO categories(id, title, created_at)
+        values('${id}', 'free', 'now()') 
+    `,
+  );
+
+  await AppDataSource.destroy();
+}
+
+createFreeCategory().then(() =>
+  console.log('Default category "free" was created.'),
+);
+
+export { createFreeCategory };
