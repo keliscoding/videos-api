69a225ec71e4b49bb24bc4e847fdf7ed6597d05f
Kelly Castelo <kawokkun@gmail.com>
2022-06-17 09:55:55 -0300

    test: add filter video by title use case test

--- src/modules/videos/useCases/FindAllVideos/FindAllVideosUseCase.test.ts
+++ src/modules/videos/useCases/FindAllVideos/FindAllVideosUseCase.test.ts
@@ -28,4 +28,23 @@ describe('Find All Videos', () => {
 
     expect(allVideos).toHaveLength(2);
   });
+
+  it('Should be able to filter videos by its title', async () => {
+    await videosRepositoryInMemory.create({
+      title: 'pumpkin test video',
+      description: 'random description',
+      url: 'http://www.video.com/pumpkin',
+    });
+
+    await videosRepositoryInMemory.create({
+      title: 'grape test video',
+      description: 'random description',
+      url: 'http://www.video.com/grape',
+    });
+
+    const filteredVideo = await findAllVideosUseCase.execute('pump');
+
+    expect(filteredVideo).toHaveLength(1);
+    expect(filteredVideo).toMatchObject([{ title: 'pumpkin test video' }]);
+  });
 });
