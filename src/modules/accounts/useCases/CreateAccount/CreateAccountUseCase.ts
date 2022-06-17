86ddfbeb159a87302d4ec0cd1029d962db745cea
Kelly Castelo <kawokkun@gmail.com>
2022-06-17 13:00:05 -0300

    feat: implements create use account use case

---
+++ src/modules/accounts/useCases/CreateAccount/CreateAccountUseCase.ts
@@ -0,0 +1,36 @@
+import { AppError } from '@errors/AppError';
+import { Account } from '@modules/accounts/infra/typeorm/entities/Account';
+import { IAccountRepository } from '@modules/accounts/repositories/IAccountRepository';
+import { CreateAccountDTO } from '@shared/dtos/CreateAccountDTO';
+
+class CreateAccountUseCase {
+  constructor(private accountRepository: IAccountRepository) {}
+
+  async execute({
+    username,
+    password,
+    email,
+  }: CreateAccountDTO): Promise<Account> {
+    if (!username) throw new AppError('missing username field');
+    if (!password) throw new AppError('missing password field');
+    if (!email) throw new AppError('missing email field');
+
+    const usernameExists = await this.accountRepository.findAccountByUsername(
+      username,
+    );
+    if (usernameExists) throw new AppError('username already taken');
+
+    const emailExists = await this.accountRepository.findAccountByEmail(email);
+    if (emailExists) throw new AppError('email already taken');
+
+    const account = await this.accountRepository.create({
+      username,
+      password,
+      email,
+    });
+
+    return account;
+  }
+}
+
+export { CreateAccountUseCase };
